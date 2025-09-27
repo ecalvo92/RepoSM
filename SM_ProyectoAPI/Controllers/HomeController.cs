55383c760440c67cc19fb09f570a45d6fbe786bb
Eduardo Calvo Castillo <edcalvo92@hotmail.com>
2025-09-27 10:49:28 -0600

    Se inicia con el proyecto api

---
+++ SM_ProyectoAPI/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SM_ProyectoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost]
+        [Route("IniciarSesion")]
+        public IActionResult IniciarSesion()
+        {
+            return Ok();
+        }
+
+    }
+}
