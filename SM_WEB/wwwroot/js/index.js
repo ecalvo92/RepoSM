216c554b6fc18037f0eeede4f9bfa543b307e995
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-06-20 10:08:55 -0600

    Validation FrontEnd

---
+++ SM_WEB/wwwroot/js/index.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+  $("#IndexForm").validate({
+        rules: {
+            CorreoElectronico: {
+                required: true,
+                email: true
+            },
+            Contrasenna: {
+                required: true,
+                minlength: 5
+            }
+        },
+        messages: {
+            CorreoElectronico: {
+                required: "Campo obligatorio.",
+                email: "Formato no válido."
+            },
+            Contrasenna: {
+                required: "Campo obligatorio.",
+                minlength: "Mínimo 5 caracteres."
+            }
+        },
+        errorElement: "span",
+        errorPlacement: function (error, element) {
+            error.addClass("text-danger small");
+            error.insertAfter(element.closest(".form-group"));
+        },
+        highlight: function (element) {
+            $(element).addClass("is-invalid").removeClass("is-valid");
+        },
+        unhighlight: function (element) {
+            $(element).removeClass("is-invalid").addClass("is-valid");
+        }
+    });
+});
---
+++ SM_WEB/wwwroot/js/registro.js
@@ -0,0 +1,47 @@
+﻿$(document).ready(function () {
+  $("#RegistroForm").validate({
+    rules: {
+      Identificacion: {
+        required: true
+      },
+      Nombre: {
+        required: true
+      },
+      CorreoElectronico: {
+        required: true,
+        email: true
+      },
+      Contrasenna: {
+        required: true,
+        minlength: 5
+      }
+    },
+    messages: {
+      Identificacion: {
+        required: "Campo obligatorio."
+      },
+      Nombre: {
+        required: "Campo obligatorio."
+      },
+      CorreoElectronico: {
+        required: "Campo obligatorio.",
+        email: "Formato no válido."
+      },
+      Contrasenna: {
+        required: "Campo obligatorio.",
+        minlength: "Mínimo 5 caracteres."
+      }
+    },
+    errorElement: "span",
+    errorPlacement: function (error, element) {
+      error.addClass("text-danger small");
+      error.insertAfter(element.closest(".form-group"));
+    },
+    highlight: function (element) {
+      $(element).addClass("is-invalid").removeClass("is-valid");
+    },
+    unhighlight: function (element) {
+      $(element).removeClass("is-invalid").addClass("is-valid");
+    }
+  });
+});
