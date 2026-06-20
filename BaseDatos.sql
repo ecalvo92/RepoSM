cd4dd58534a3576eba0078f7d2bfd7dbeaca733d
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-06-20 10:52:16 -0600

    Update BaseDatos.sql

--- BaseDatos.sql
+++ BaseDatos.sql
@@ -42,6 +42,9 @@ INSERT [dbo].[tbError] ([Consecutivo], [Mensaje], [Lugar], [FechaHora], [Consecu
 GO
 INSERT [dbo].[tbError] ([Consecutivo], [Mensaje], [Lugar], [FechaHora], [ConsecutivoUsuario]) VALUES (3, N'Procedure or function ''spIniciarSesionUsuario'' expects parameter ''@CorreoElectronico'', which was not supplied.', N'/api/Home/IniciarSesionAPI', CAST(N'2026-06-13T10:44:28.530' AS DateTime), 0)
 GO
+INSERT [dbo].[tbError] ([Consecutivo], [Mensaje], [Lugar], [FechaHora], [ConsecutivoUsuario]) VALUES (4, N'Violation of UNIQUE KEY constraint ''UK_Identificacion''. Cannot insert duplicate key in object ''dbo.tbUsuario''. The duplicate key value is (402500603).
+The statement has been terminated.', N'/api/Home/RegistroAPI', CAST(N'2026-06-20T08:49:42.663' AS DateTime), 0)
+GO
 SET IDENTITY_INSERT [dbo].[tbError] OFF
 GO
 
@@ -52,6 +55,18 @@ GO
 INSERT [dbo].[tbUsuario] ([Consecutivo], [Identificacion], [Nombre], [CorreoElectronico], [Contrasenna], [Estado]) VALUES (2, N'402500603', N'LEON CORDERO ESTEFAN', N'eleon00603@ufide.ac.cr', N'00603', 1)
 GO
 SET IDENTITY_INSERT [dbo].[tbUsuario] OFF
+GO
+
+ALTER TABLE [dbo].[tbUsuario] ADD  CONSTRAINT [UK_CorreoElectronico] UNIQUE NONCLUSTERED 
+(
+	[CorreoElectronico] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[tbUsuario] ADD  CONSTRAINT [UK_Identificacion] UNIQUE NONCLUSTERED 
+(
+	[Identificacion] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
 
 CREATE PROCEDURE [dbo].[spIniciarSesionUsuario]
@@ -94,11 +109,17 @@ CREATE PROCEDURE [dbo].[spRegistrarUsuario]
     @Contrasenna        varchar(100)
 AS
 BEGIN
+
+    IF NOT EXISTS (SELECT 1 FROM tbUsuario
+                   WHERE Identificacion = @Identificacion
+                    OR   CorreoElectronico = @CorreoElectronico)
+    BEGIN
 	
-    DECLARE @Estado BIT = 1
+        DECLARE @Estado BIT = 1
 
-    INSERT INTO dbo.tbUsuario (Identificacion,Nombre,CorreoElectronico,Contrasenna,Estado)
-    VALUES (@Identificacion,@Nombre,@CorreoElectronico,@Contrasenna,@Estado)
+        INSERT INTO dbo.tbUsuario (Identificacion,Nombre,CorreoElectronico,Contrasenna,Estado)
+        VALUES (@Identificacion,@Nombre,@CorreoElectronico,@Contrasenna,@Estado)
 
+    END
 END
 GO
